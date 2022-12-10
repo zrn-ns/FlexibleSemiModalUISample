f6086049d66d02cdc8a5212962f13851715e6078
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2022-12-10 13:49:59 +0900

    Initial Commit

---
+++ FlexibleSemiModalUISample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlexibleSemiModalUISample
+//
+//  Created by zrn_ns on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
